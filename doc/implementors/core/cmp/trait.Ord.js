5190eda36c5bd27e76df147e6f59f241a0fe0d28
Stanislav Panferov <fnight.m@gmail.com>
2015-02-12 17:17:12 +0300

    sg

---
+++ doc/implementors/core/cmp/trait.Ord.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors['log'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='enum' href='log/enum.LogLevel.html' title='log::LogLevel'>LogLevel</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/cmp/trait.Ord.html' title='core::cmp::Ord'>Ord</a> for <a class='enum' href='log/enum.LogLevelFilter.html' title='log::LogLevelFilter'>LogLevelFilter</a>",];
+implementors['phf'] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ doc/implementors/rand/trait.Rand.js
@@ -0,0 +1,11 @@
+(function() {var implementors = {};
+implementors['rand'] = [];
+implementors['uuid'] = ["<a class='stability Unstable' title='Unstable: use the crates.io `rand` library instead'></a>impl <a class='trait' href='rand/trait.Rand.html' title='rand::Rand'>Rand</a> for <a class='struct' href='uuid/struct.Uuid.html' title='uuid::Uuid'>Uuid</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
