8289e3c9f77f50ee808985dcdddda9a2b5928d11
Stanislav Panferov <fnight.m@gmail.com>
2015-02-20 07:39:20 +0000

    rebuild pages at b5cba1bac5f1c6c0d2557c7668027f59bdfb13fc

--- doc/implementors/rand/trait.Rand.js
+++ doc/implementors/rand/trait.Rand.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
 implementors['rand'] = [];
-implementors['uuid'] = ["<a class='stability Unstable' title='Unstable: use the crates.io `rand` library instead'></a>impl <a class='trait' href='rand/trait.Rand.html' title='rand::Rand'>Rand</a> for <a class='struct' href='uuid/struct.Uuid.html' title='uuid::Uuid'>Uuid</a>",];
+implementors['uuid'] = ["<a class='stability Deprecated' title='Deprecated: use the crates.io `rand` library instead'></a>impl <a class='trait' href='rand/trait.Rand.html' title='rand::Rand'>Rand</a> for <a class='struct' href='uuid/struct.Uuid.html' title='uuid::Uuid'>Uuid</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
