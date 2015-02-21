3bb049db995882cd2cee4153d1d0dbe22ef3ee16
Stanislav Panferov <fnight.m@gmail.com>
2015-02-21 09:41:36 +0000

    rebuild pages at b7a8e030480472a653e7db29da5ac447aa21a466

--- doc/implementors/core/marker/trait.Send.js
+++ doc/implementors/core/marker/trait.Send.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
 implementors['phf'] = [];
-implementors['valico'] = ["<a class='stability Unstable' title='Unstable: will be overhauled with new lifetime rules; see RFC 458'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='valico/json_dsl/struct.Builder.html' title='valico::json_dsl::Builder'>Builder</a>","<a class='stability Unstable' title='Unstable: will be overhauled with new lifetime rules; see RFC 458'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='valico/json_dsl/struct.Param.html' title='valico::json_dsl::Param'>Param</a>",];
+implementors['valico'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='valico/json_dsl/struct.Builder.html' title='valico::json_dsl::Builder'>Builder</a>","<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/marker/trait.Send.html' title='core::marker::Send'>Send</a> for <a class='struct' href='valico/json_dsl/struct.Param.html' title='valico::json_dsl::Param'>Param</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
