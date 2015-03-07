af9dcb22db37f42b9465ecd4890ef10bdd82ce81
Stanislav Panferov <fnight.m@gmail.com>
2015-03-07 07:03:05 +0000

    rebuild pages at 30a810df3978702e1840b3ec7ae4695a9b2951e6

--- doc/implementors/core/default/trait.Default.js
+++ doc/implementors/core/default/trait.Default.js
@@ -1,6 +1,5 @@
 (function() {var implementors = {};
-implementors['rand'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='rand/reseeding/struct.ReseedWithDefault.html' title='rand::reseeding::ReseedWithDefault'>ReseedWithDefault</a>",];
-implementors['uuid'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='uuid/struct.Uuid.html' title='uuid::Uuid'>Uuid</a>",];
+implementors['rand'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='rand/reseeding/struct.ReseedWithDefault.html' title='rand::reseeding::ReseedWithDefault'>ReseedWithDefault</a>",];implementors['uuid'] = ["<a class='stability Stable' title='Stable'></a>impl <a class='trait' href='http://doc.rust-lang.org/nightly/core/default/trait.Default.html' title='core::default::Default'>Default</a> for <a class='struct' href='uuid/struct.Uuid.html' title='uuid::Uuid'>Uuid</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
