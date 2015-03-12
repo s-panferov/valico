8b3bf840bee74ec1e46a845093ee6f86c703b38e
Stanislav Panferov <fnight.m@gmail.com>
2015-03-12 07:33:45 +0000

    rebuild pages at 137d7a7fa0a5d787757ab347fb7a714025c5de3f

--- doc/valico/json_schema/schema/sidebar-items.js
+++ doc/valico/json_schema/schema/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["CompilationSettings",""],["Schema",""],["ScopedSchema",""],["WalkContext",""]],"enum":[["SchemaError",""]],"fn":[["compile",""]]});
+initSidebarItems({"fn":[["compile",""]],"enum":[["SchemaError",""]],"struct":[["CompilationSettings",""],["Schema",""],["ScopedSchema",""],["WalkContext",""]]});
