a6499a04cb2d08907ebd86875b03d8e41b5f8863
Stanislav Panferov <fnight.m@gmail.com>
2015-04-16 07:02:20 +0000

    rebuild pages at 4b280fc2ec701ad1f2f56b2abaeff795b1dc7564

--- doc/libc/types/os/common/posix01/sidebar-items.js
+++ doc/libc/types/os/common/posix01/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["timezone",""]],"type":[["pthread_t",""],["rlim_t",""],["sighandler_t",""]],"struct":[["glob_t",""],["rlimit",""],["timespec",""],["timeval",""]]});
+initSidebarItems({"type":[["pthread_t",""],["rlim_t",""],["sighandler_t",""]],"struct":[["glob_t",""],["rlimit",""],["timespec",""],["timeval",""]],"enum":[["timezone",""]]});
