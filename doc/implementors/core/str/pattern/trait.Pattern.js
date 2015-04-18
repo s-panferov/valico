f9f321b2e08db0a046b9f6320c86d4d091ca8143
Stanislav Panferov <fnight.m@gmail.com>
2015-04-18 06:56:01 +0000

    rebuild pages at 4b280fc2ec701ad1f2f56b2abaeff795b1dc7564

--- doc/implementors/core/str/pattern/trait.Pattern.js
+++ doc/implementors/core/str/pattern/trait.Pattern.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['regex'] = ["<a class='stability Unstable' title='Unstable'></a>impl&lt;'r, 't&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/str/pattern/trait.Pattern.html' title='core::str::pattern::Pattern'>Pattern</a>&lt;'t&gt; for &amp;'r <a class='enum' href='regex/enum.Regex.html' title='regex::Regex'>Regex</a>",];
+implementors['regex'] = ["impl&lt;'r, 't&gt; <a class='trait' href='http://doc.rust-lang.org/nightly/core/str/pattern/trait.Pattern.html' title='core::str::pattern::Pattern'>Pattern</a>&lt;'t&gt; for &amp;'r <a class='enum' href='regex/enum.Regex.html' title='regex::Regex'>Regex</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
