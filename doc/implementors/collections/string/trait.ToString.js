1e0caf80b98e7fa23f1965158682dc26bb8ffa6c
Stanislav Panferov <fnight.m@gmail.com>
2016-08-14 11:16:41 +0000

    rebuild pages at 53e6d517afd10d49b5a0f8bd75d8a076d5b29a84

--- doc/implementors/collections/string/trait.ToString.js
+++ doc/implementors/collections/string/trait.ToString.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['regex_syntax'] = [];
+implementors["regex_syntax"] = [];implementors["serde"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- doc/implementors/core/str/pattern/trait.DoubleEndedSearcher.js
+++ doc/implementors/core/str/pattern/trait.DoubleEndedSearcher.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors['libc'] = [];implementors['regex_syntax'] = [];
+implementors["libc"] = [];implementors["lazy_static"] = [];implementors["regex_syntax"] = [];implementors["serde"] = [];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
